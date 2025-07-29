f178a01f6ee0f1ce87bfb1b9cc57d8de177646df
raze <razea1.the.master@gmail.com>
2025-07-30 01:20:36 +0300

    loading screen

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./styles/index.css"; // ✅ updated path
+import LoadingPage from './pages/loadingpage.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <LoadingPage /> 
   </React.StrictMode>,
-)
+);
+
