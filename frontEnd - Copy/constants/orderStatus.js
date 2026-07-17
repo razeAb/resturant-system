8f9dff7eb6b2e08ba5064f83cf603cfac3a5f5f3
raze <razea1.the.master@gmail.com>
2026-07-17 11:38:44 +0300

    removced uselsess data

--- frontEnd - Copy/constants/orderStatus.js
+++
@@ -1,9 +0,0 @@
-export const ORDER_STATUS = {
-  PENDING: "pending",
-  PENDING_PAYMENT: "pending_payment",
-  PAID: "paid",
-  PREPARING: "preparing",
-  DELIVERING: "delivering",
-  DONE: "done",
-  CANCELED: "canceled",
-};
